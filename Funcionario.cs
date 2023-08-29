c0a3c0894d1043806bcc5fa4dff6b0771c435ab5
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-08-29 13:55:02 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDeFixacao_Listas
+{
+    internal class Funcionario
+    {
+        public int Cod { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int cod, string nome, double salario) 
+        { 
+            Cod = cod;
+            Nome = nome;
+            Salario = salario;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ExercicioDeFixacao_Listas;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Empresa Focus S/A");
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine();
+        Console.Write("Quantos funcionários você quer cadastrar?");
+        int num = int.Parse(Console.ReadLine());
+
+        List<Funcionario> list = new List<Funcionario>();
+
+        for (int i = 1; i < num; i++)
+        {
+            Console.WriteLine("Funcionário #" + i + ":");
+            Console.Write("Código: ");
+            int cod = int.Parse(Console.ReadLine());
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            list.Add(new Funcionario(cod, nome, salario));
+            Console.WriteLine();
+        }
+    }
+}
